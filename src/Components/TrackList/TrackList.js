04086366b810699d994d4ed8062fb8faabeea041
jeromebjerome <navig@laposte.net>
2018-04-26 22:06:30 +0200

    Add files via upload

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,1 +1,25 @@
+import React from 'react';
 
+import {Track} from './../Track/Track';
+
+import './TrackList.css';
+
+export class TrackList extends React.Component {
+
+    render() {
+        if(this.props.tracks) {
+            return(
+                <div className="TrackList">
+                {
+                    this.props.tracks.map(track => {
+                        return <Track key={track.id} track={track} onAdd={this.props.onAdd} onRemove={this.props.onRemove} isRemoval={this.props.isRemoval}/>
+                    })
+                }
+                </div>
+            );
+        }
+        else {
+            return null;
+        }
+    };
+};
